7d9393f6297a22cdf9102269454805632ae545a4
author name <author email>
2024-12-12 09:34:27 +0000

    chore(action) 🦈: today's daily leetcode question added

---
+++ Daily Problems/December 2024/2558. Take Gifts From the Richest Pile/solution.cpp
@@ -0,0 +1,19 @@
+
+//
+// Created by SHANTANU on 2024-12-12.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        
+    }
+};
+
+int main() {
+    Solution solution;
+
+    return 0;
+}
